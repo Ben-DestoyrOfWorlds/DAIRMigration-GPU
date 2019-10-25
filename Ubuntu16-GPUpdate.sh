97121587fe80fb15892e769c2a528d2e6348cb52
Ben-DestoyrOfWorlds <52978856+Ben-DestoyrOfWorlds@users.noreply.github.com>
2019-10-25 12:50:00 -0600

    uname instead of ls

--- Ubuntu16-GPUpdate.sh
+++ Ubuntu16-GPUpdate.sh
@@ -20,19 +20,19 @@ alias nouveau off
 alias lbm-nouveau off
 EOF
 
-echo blacklist nouveau | sudo tee -a /etc/modprobe.d/blacklist.conf
+echo blacklist nouveau | tee -a /etc/modprobe.d/blacklist.conf
 
 rmmod nouveau
 
-echo options nouveau modeset=0 | sudo tee -a /etc/modprobe.d/nouveau-kms.conf
+echo options nouveau modeset=0 | tee -a /etc/modprobe.d/nouveau-kms.conf
 
-sudo update-initramfs -u
+update-initramfs -u
 
 # nvidia drivers, cuda
 
-sudo apt-get update
+apt-get update
 # Be extra explicit about noninteractive.
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y linux-image-extra-virtual linux-headers-generic build-essential unzip dkms
+DEBIAN_FRONTEND=noninteractive apt-get install -y linux-image-extra-virtual linux-headers-generic build-essential unzip dkms
 
 # Install vGPU Driver
 echo " ====> Downloading vGPU Driver"
@@ -40,10 +40,10 @@ wget -q https://swift-yyc.cloud.cybera.ca:8080/v1/AUTH_8c4974ed39a44c2fabd9d7589
 unzip NVIDIA-GRID-Linux-KVM-410.92-410.91-412.16.zip
 chmod +x *.run
 
-sudo apt-get install -y nvidia-modprobe
+apt-get install -y nvidia-modprobe
 
 echo " ====> Installing vGPU Driver"
-sudo ./NVIDIA-Linux-x86_64-410.92-grid.run --dkms -as -k $(ls /boot | grep vmlinuz | tail -n 1 | sed 's/vmlinuz-//')
+./NVIDIA-Linux-x86_64-410.92-grid.run --dkms -as -k `uname -r`
 
 # Cleanup NVIDIA
 rm -rf *.pdf
@@ -53,7 +53,7 @@ rm -rf *.run
 
 # Set up licensing
 mkdir -p /etc/nvidia
-cat << EOF | sudo tee /etc/nvidia/gridd.conf
+cat << EOF | tee /etc/nvidia/gridd.conf
 ServerAddress=nvidia.dair-atir.canarie.ca
 ServerPort=7070
 FeatureType=2
@@ -65,24 +65,24 @@ echo " ====> Downloading CUDA"
 #10.1 not supported by latest vGPU driver (410.92)
 #wget -q https://developer.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_10.1.105_418.39_linux.run
 wget -q https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda_10.0.130_410.48_linux
-sudo chmod +x cuda_*
+chmod +x cuda_*
 
 echo " ====> Installing CUDA"
-sudo ./cuda* --silent --toolkit --samplespath=/usr/local/cuda/samples
+./cuda* --silent --toolkit --samplespath=/usr/local/cuda/samples
 
-cat << EOF | sudo tee /etc/ld.so.conf.d/ld-library.conf
+cat << EOF | tee /etc/ld.so.conf.d/ld-library.conf
 # Add CUDA to LD
 /usr/local/cuda/lib64
 EOF
 
-sudo ldconfig
+ldconfig
 
 # Clean up
 cd
-sudo rm -rf cuda*
-sudo rm -rf /root/NVIDIA*
+rm -rf cuda*
+rm -rf /root/NVIDIA*
 
-sudo apt-get clean
+apt-get clean
 
 #Ensure changes are written to disk
 sync
