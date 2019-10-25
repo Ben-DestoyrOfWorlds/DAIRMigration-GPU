0be1859d36d298a89b61c956406cc34573b3f4e9
Ben-DestoyrOfWorlds <52978856+Ben-DestoyrOfWorlds@users.noreply.github.com>
2019-10-25 13:22:39 -0600

    back to the complicated

--- Ubuntu16-GPUpdate.sh
+++ Ubuntu16-GPUpdate.sh
@@ -43,7 +43,7 @@ chmod +x *.run
 apt-get install -y nvidia-modprobe
 
 echo " ====> Installing vGPU Driver"
-./NVIDIA-Linux-x86_64-410.92-grid.run --dkms -as -k `uname -r`
+./NVIDIA-Linux-x86_64-410.92-grid.run --dkms -as -k $(ls /boot | grep vmlinuz | tail -n 1 | sed 's/vmlinuz-//')
 
 # Cleanup NVIDIA
 rm -rf *.pdf
